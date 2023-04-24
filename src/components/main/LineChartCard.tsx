25e5a3f83042d9dddfd7f5be48dcd6e153974ab5
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 10:47:23 +0100

    created line chart card component

---
+++ src/components/main/LineChartCard.tsx
@@ -0,0 +1,20 @@
+import info from "../../assets/icons/info.svg";
+
+const LineChartCard = () => {
+  return (
+    <div className="w-full border-2 rounded-md p-3 my-6 border-[#EFF1F6]">
+      <div className="flex justify-between">
+        <div className="">
+          <h2 className="text-xl font-bold">Page Views</h2>
+          <p>All time</p>
+        </div>
+        <button>
+          <img src={info} alt="info" />
+        </button>
+      </div>
+      <h1 className="text-4xl font-bold mt-3">500</h1>
+    </div>
+  );
+};
+
+export default LineChartCard;
