f959719cc379d8d940afd3d0b12116c197a1b246
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 12:29:09 +0100

    created top referral sources components

---
+++ src/components/main/charts/pie_chart_cards/TopReferralSources.tsx
@@ -0,0 +1,11 @@
+import PieChartCard from "./PieChartCard";
+
+const TopReferralSources = () => {
+  return (
+    <PieChartCard title="Top Referral Sources">
+      <div className=""></div>
+    </PieChartCard>
+  );
+};
+
+export default TopReferralSources;
