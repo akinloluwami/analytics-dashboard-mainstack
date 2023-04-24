0827507796dbcc92e80ee10d9b37795a1bb3a207
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 12:18:57 +0100

    changed to button

--- src/components/main/PieChartCard.tsx
+++ src/components/main/PieChartCard.tsx
@@ -7,9 +7,7 @@ const PieChartCard = ({ title }: PieChartCardProps) => {
     <div className="w-1/2 border-2 border-light_gray p-5 rounded-md">
       <div className="flex items-center justify-between">
         <h1 className="font-bold">{title}</h1>
-        <p className="text-accent cursor-pointer font-semibold">
-          View full reports
-        </p>
+        <button className="text-accent font-semibold">View full reports</button>
       </div>
     </div>
   );
