bd5b4a28797493c9f8a242f87bc13f9856197f16
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 14:39:32 +0100

    changed color name

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Main from "./components/main/Main";
 const App = () => {
   return (
     <div className="flex overflow-y-scroll">
-      <div className="w-1/5 bg-white py-5 fixed border-l border-[#EFF1F6]">
+      <div className="w-1/5 bg-white py-5 fixed border-l border-light_gray">
         <Sidebar />
       </div>
       <div className="w-4/5 ml-[20%] px-10 py-5">
