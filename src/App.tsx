c87570889beb9364db8001e32eef67fb4b6fe9bd
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 10:16:27 +0100

    Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/Main";
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const res = await axios.get("https://fe-task-api.mainstack.io");
-        setData(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-  }, []);
-
   return (
     <div className="h-screen flex overflow-y-scroll">
       <div className="w-1/5 bg-white py-5 fixed border-l border-[#EFF1F6]">
         <Sidebar />
       </div>
-      <div className="w-4/5 ml-[20%] px-10 py-5">hey</div>
+      <div className="w-4/5 ml-[20%] px-10 py-5">
+        <Main />
+      </div>
     </div>
   );
 };
