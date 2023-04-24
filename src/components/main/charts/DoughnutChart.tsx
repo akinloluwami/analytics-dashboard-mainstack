c7774ad5fc8785d3d093384ee765ffc0ced05960
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 13:33:24 +0100

    added width and height

--- src/components/main/charts/DoughnutChart.tsx
+++ src/components/main/charts/DoughnutChart.tsx
@@ -1,10 +1,27 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip);
+
+export const options = {
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
 
 const DoughnutChart = ({ data }: any) => {
-  return <Doughnut data={data} />;
+  return (
+    <Doughnut
+      data={data}
+      options={options}
+      style={{
+        width: "250px",
+        height: "250px",
+      }}
+    />
+  );
 };
 
 export default DoughnutChart;
