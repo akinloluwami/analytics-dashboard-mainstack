79fe7204cc68c90e8d818e95a37289c393f840cd
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 09:47:41 +0100

    items

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -26,11 +26,37 @@ const Sidebar = () => {
       icon: hourglass_empty,
     },
   ];
+  const otherItems = [
+    {
+      title: "Item 4",
+      icon: dashboard,
+    },
+    {
+      title: "Item 5",
+      icon: edit,
+    },
+  ];
+  const otherItems2 = [
+    {
+      title: "Item 6",
+      icon: dashboard,
+    },
+    {
+      title: "Item 7",
+      icon: edit,
+    },
+    {
+      title: "Item 8",
+      icon: edit,
+    },
+  ];
   return (
     <div>
       <img src={mainstackLogo} className="ml-8" alt="logo" />
       <div className="">
         <SidebarItems items={mainItems} />
+        <SidebarItems items={otherItems} title="Others 1" />
+        <SidebarItems items={otherItems2} title="Others 2" />
       </div>
     </div>
   );
