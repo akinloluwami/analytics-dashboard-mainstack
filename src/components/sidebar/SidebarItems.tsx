ba15d2c29ab9b2d3e07446af9afc1c258e5bb0c6
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 09:26:32 +0100

    __

--- src/components/sidebar/SidebarItems.tsx
+++ src/components/sidebar/SidebarItems.tsx
@@ -1,14 +1,21 @@
 interface SidebarItemProps {
   title: string;
   icon: string;
+  active?: boolean;
 }
 
 const SidebarItems = ({ items }: { items: SidebarItemProps[] }) => {
   return (
     <div className="">
       {items.map((item) => (
-        <div className="" key={crypto.randomUUID()}>
-          <img src={item.icon} /> {item.title}
+        <div
+          className={`flex ${item.active && "active-link"}`}
+          key={crypto.randomUUID()}
+        >
+          <img src={item.icon} className="ml-8" />{" "}
+          <span className="font-semibold text-lg items-center ml-2">
+            {item.title}
+          </span>
         </div>
       ))}
     </div>
