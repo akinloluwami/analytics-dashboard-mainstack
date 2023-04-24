ec7a9a33cea6045f67235d4d2e5f5b9a3bebbfaa
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 11:42:17 +0100

    added props

--- src/components/main/LineChartCard.tsx
+++ src/components/main/LineChartCard.tsx
@@ -1,7 +1,8 @@
 import info from "../../assets/icons/info.svg";
+import IgraphData from "../../typings/graph-data";
 import LineChart from "./charts/LineChart";
 
-const LineChartCard = () => {
+const LineChartCard = ({ graphData }: { graphData: IgraphData }) => {
   return (
     <div className="w-full border-2 rounded-md p-3 my-6 border-[#EFF1F6]">
       <div className="flex justify-between">
@@ -15,7 +16,7 @@ const LineChartCard = () => {
       </div>
       <h1 className="text-4xl font-bold mt-3">500</h1>
 
-      <LineChart />
+      <LineChart graphData={graphData} />
     </div>
   );
 };
