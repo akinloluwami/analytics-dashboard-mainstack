7205122521ecccabc70063189142a43df9f5182d
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 10:15:25 +0100

    main

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
       <div className="w-1/5 bg-white py-5 fixed border-l border-[#EFF1F6]">
         <Sidebar />
       </div>
-      <div className="w-4/5 ml-[20%]">hey</div>
+      <div className="w-4/5 ml-[20%] px-10 py-5">hey</div>
     </div>
   );
 };
