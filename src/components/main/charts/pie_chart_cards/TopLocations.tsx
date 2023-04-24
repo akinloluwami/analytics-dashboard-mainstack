4c4dd92801c38417bc6720ca6815a8710aeaec03
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 12:28:48 +0100

    created top locations component

---
+++ src/components/main/charts/pie_chart_cards/TopLocations.tsx
@@ -0,0 +1,11 @@
+import PieChartCard from "./PieChartCard";
+
+const TopLocations = () => {
+  return (
+    <PieChartCard title="Top Locations">
+      <div className=""></div>
+    </PieChartCard>
+  );
+};
+
+export default TopLocations;
