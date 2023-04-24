824cf04733e946baa6bdab5c840ad0ec297bfce1
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 11:50:10 +0100

    fixed typings

--- src/components/main/LineChartCard.tsx
+++ src/components/main/LineChartCard.tsx
@@ -2,22 +2,33 @@ import info from "../../assets/icons/info.svg";
 import IgraphData from "../../typings/graph-data";
 import LineChart from "./charts/LineChart";
 
-const LineChartCard = ({ graphData }: { graphData: IgraphData }) => {
-  return (
-    <div className="w-full border-2 rounded-md p-3 my-6 border-[#EFF1F6]">
-      <div className="flex justify-between">
-        <div className="">
-          <h2 className="text-xl font-bold">Page Views</h2>
-          <p>All time</p>
-        </div>
-        <button>
-          <img src={info} alt="info" />
-        </button>
-      </div>
-      <h1 className="text-4xl font-bold mt-3">500</h1>
+interface LineChartCardProps {
+  graphData?: IgraphData;
+}
 
-      <LineChart graphData={graphData} />
-    </div>
+const LineChartCard = ({
+  graphData = {
+    views: {},
+  },
+}: LineChartCardProps) => {
+  return (
+    <>
+      {graphData && (
+        <div className="w-full border-2 rounded-md p-3 my-6 border-[#EFF1F6]">
+          <div className="flex justify-between">
+            <div className="">
+              <h2 className="text-xl font-bold">Page Views</h2>
+              <p>All time</p>
+            </div>
+            <button>
+              <img src={info} alt="info" />
+            </button>
+          </div>
+          <h1 className="text-4xl font-bold mt-3">500</h1>
+          <LineChart graphData={graphData} />
+        </div>
+      )}
+    </>
   );
 };
 
