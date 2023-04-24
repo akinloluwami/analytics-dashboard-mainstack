f4e242a765b42c8faac210d4d31a9e9a09ecba9b
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 12:17:33 +0100

    added "view full reports" p

--- src/components/main/PieChartCard.tsx
+++ src/components/main/PieChartCard.tsx
@@ -6,7 +6,10 @@ const PieChartCard = ({ title }: PieChartCardProps) => {
   return (
     <div className="w-1/2 border-2 border-light_gray p-5 rounded-md">
       <div className="flex items-center justify-between">
-        <h1>{title}</h1>
+        <h1 className="font-bold">{title}</h1>
+        <p className="text-accent cursor-pointer font-semibold">
+          View full reports
+        </p>
       </div>
     </div>
   );
