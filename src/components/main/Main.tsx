f0e9c5ff3ef786a7b15338fa563efa5bf88a44fa
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 10:16:34 +0100

    __

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Main = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get("https://fe-task-api.mainstack.io");
+        setData(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return <div>Main</div>;
 };
 
