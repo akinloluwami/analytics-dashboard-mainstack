cfc894c146032579f8e6d9d0c6640131ad58ff84
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 12:13:16 +0100

    props

--- src/components/main/PieChartCard.tsx
+++ src/components/main/PieChartCard.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+interface PieChartCardProps {
+  title: string;
+}
 
-const PieChartCard = () => {
-  return <div>PieChartCard</div>;
+const PieChartCard = ({ title }: PieChartCardProps) => {
+  return (
+    <div className="w-1/2 border-2 border-light_gray p-5 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1>{title}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default PieChartCard;
