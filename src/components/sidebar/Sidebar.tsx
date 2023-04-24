cf499e2ddf94bf0b381e05543be369290af6201e
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-24 14:36:21 +0100

    changed icons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,16 @@
 import mainstackLogo from "../../assets/mainstack-logo.png";
 import SidebarItems from "./SidebarItems";
+import SidebarFooter from "./SidebarFooter";
 
 import dashboard from "../../assets/icons/dashboard.svg";
 import edit from "../../assets/icons/edit.svg";
 import group from "../../assets/icons/group.svg";
 import hourglass_empty from "../../assets/icons/hourglass_empty.svg";
-import SidebarFooter from "./SidebarFooter";
+import camera_add from "../../assets/icons/camera_add.svg";
+import deleteIcon from "../../assets/icons/delete.svg";
+import subs from "../../assets/icons/subs.svg";
+import file_present from "../../assets/icons/file_present.svg";
+import alarm from "../../assets/icons/alarm.svg";
 
 const Sidebar = () => {
   const mainItems = [
@@ -30,25 +35,25 @@ const Sidebar = () => {
   const otherItems = [
     {
       title: "Item 4",
-      icon: dashboard,
+      icon: camera_add,
     },
     {
       title: "Item 5",
-      icon: edit,
+      icon: deleteIcon,
     },
   ];
   const otherItems2 = [
     {
       title: "Item 6",
-      icon: dashboard,
+      icon: subs,
     },
     {
       title: "Item 7",
-      icon: edit,
+      icon: file_present,
     },
     {
       title: "Item 8",
-      icon: edit,
+      icon: alarm,
     },
   ];
   return (
